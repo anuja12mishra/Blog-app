2ebcc4060ff2bdf872030d6feb1303fd4b567268
anuja12mishra <25anujamishra12@gmail.com>
2024-12-10 20:50:51 +0530

    stage 2

--- client/src/App.js
+++ client/src/App.js
@@ -2,31 +2,22 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Post from './Post';
 import Header from './Header';
+import Layout from './Layout';
+import IndexPage from './Pages/IndexPage'; 
+import LoginPage from './Pages/LoginPage';
+import RegisterPage from './Pages/RegisterPage';
 
 function App() {
   return (
     <Routes>
-      {/* Index route for the main content */}
-      <Route
-        index 
-        element={
-          <main>
-            <Header />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-          </main>
-        }
-      />
-      {/* Login route */}
-      <Route path={'/login'} element={
-          <main>
-            <Header />
-            <div>Login Page</div>
-          </main>
-        } 
-      />
+      {/* base tempelete */}
+      <Route path="/" element={<Layout />}>
+        {/* Index route for the main content */}
+        <Route index element={<IndexPage />}/>
+        {/* Login route */}
+        <Route path={'/login'} element={<LoginPage />} />
+        <Route path={'/Register'} element={<RegisterPage />} />
+      </Route>
     </Routes>
   );
 }
